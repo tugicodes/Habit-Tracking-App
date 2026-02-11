8bdd4bf630f7bbb309f4f21ed7f2ab3818d1b3cc
tugicodes <alvinkirimiedu@gmail.com>
2026-02-11 16:12:45 +0300

    welcome page first draft created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navigation from './Components/NavBar'
+import WelcomePage from  './Components/WelcomePage'
 
 function App() {
 
   return (
     <>
     <Navigation/>
+    <WelcomePage/>
       
     </>
   );
---
+++ src/Components/WelcomePage.jsx
@@ -0,0 +1,10 @@
+export default function WelcomePage(){
+    return(
+        <div className="welcomePage">
+            <div className="welcomeText">
+                <h1>Welcome to Habitual!</h1>
+                <p>Track your habits and achieve your goals with us.</p>
+            </div>
+            </div>
+    )
+}
